b188dc99aecbfea592cacc8b9383b2c031ba3f27
jesus.daniel.medina.cruz <jesus.daniel.medina.cruz@gmail.com>
2025-05-20 00:55:36 +0000

    Index updated

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,6 +1,6 @@
 import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
-export const headerData = {
+const headerDataTemplate = {
   links: [
     {
       text: 'Homes',
@@ -118,7 +118,12 @@ export const headerData = {
   actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
 };
 
-export const footerData = {
+export const headerData = {
+  links: [],
+  actions: [],
+};
+
+export const footerDataTemplate = {
   links: [
     {
       title: 'Product',
@@ -180,4 +185,11 @@ export const footerData = {
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
     Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
   `,
+};
+
+export const footerData = {
+  links: [],
+  secondaryLinks: [],
+  socialLinks: [],
+  footNote: '',
 };
